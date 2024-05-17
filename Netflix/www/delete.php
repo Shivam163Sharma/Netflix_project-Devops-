fd467872a9ecf6d1f840267e594cf4e575383bdb
SHIVAM SHARMA <122388755+Shivam163Sharma@users.noreply.github.com>
2024-05-17 18:59:28 +0530

    Update delete.php

--- Netflix/www/delete.php
+++ Netflix/www/delete.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_GET['id'];
-$con = mysqli_connect("localhost","root","","login");
+$con = mysqli_connect("db","shivam","shivam","login");
 $sql = "DELETE FROM `images` WHERE `id`='$id'";
 $result = mysqli_query($con, $sql);
 if($result)
